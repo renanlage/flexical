f0a01d3e78b08d7156298698f4d1d5804bc978dc
Renan Lage <renanlage@gmail.com>
2016-08-05 14:04:22 -0300

    Rename and reorganize preprocessing

--- socal/preprocess.py
+++ socal/preprocess.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import sys, getopt, os
-import nltk, re
+import nltk
 import io
-
-from os.path import isfile, isdir, join
 
 stemmer = nltk.stem.RSLPStemmer()
 sentence_tokenizer = nltk.data.load('tokenizers/punkt/portuguese.pickle')
@@ -14,15 +11,13 @@ def preprocess_text(text, word_transforms=(), ignored_words=()):
   # Tokenize raw text by sentence and then by word
   for sentence in tokenize_sentences(text):
     for word in tokenize_words(sentence):
+      word = word.lower()
+      # Ignore given words
       if word not in ignored_words:
         # Apply word transformation for each word
         for word_transform in word_transforms:
           word = word_transform(word)
         yield word
-
-
-def word_vec_to_string(word_vec):
-  return u' '.join(word_vec)
 
 
 # File handling functions
@@ -47,19 +42,22 @@ def tokenize_words(sentence):
 
 # Word transform functions
 
-def lower_word(word):
-  return word.lower()
-
 def stem_word(word):
   return stemmer.stem(word)
+
+
+# Utils
+
+def word_vec_to_string(word_vec):
+  return u' '.join(word_vec)
 
 
 if __name__ == "__main__":
   text = raw_read('data/hotels.txt', 'latin-1')
   stopwords = nltk.corpus.stopwords.words('portuguese')
   punctuation = ['.', ',', ';', ':', '?', '!', '""', "''", "``"]
-  word_transforms = (lower_word, stem_word)
+  word_transforms = (stem_word,)
 
-  preprocessed_text = list(preprocess_text(text, word_transforms, stopwords))
+  preprocessed_text = list(preprocess_text(text, word_transforms))
   print preprocessed_text[:200]
   raw_write('output.txt', 'utf-8', word_vec_to_string(preprocessed_text))
