d1b99bf4f158c2b2e99d4e09a27950da7b1b069b
Renan Lage <renanlage@gmail.com>
2016-08-05 23:35:50 -0300

    Transform from ratings to sentiment scores

--- flexical/socal/socal.py
+++ flexical/socal/socal.py
@@ -3,20 +3,23 @@ import io
 from flexical.preprocess import preprocess_text, stem_word, remove_accents, remove_repeated_letters
 
 
-def load_hotel_reviews(filepath, stem_words=False):
+def load_hotel_reviews(stem_words=False):
     reviews_score = []
-    word_transforms = (remove_repeated_letters, remove_accents)
+    word_transforms = [remove_repeated_letters, remove_accents]
 
     if stem_words:
-        word_transforms = [stem_word].extend(word_transforms)
+        word_transforms.insert(0, stem_word)
 
-    with io.open(filepath, 'r', encoding='latin-1') as _file:
+    with io.open('flexical/data/hotels.txt', 'r', encoding='latin-1') as _file:
         while True:
             raw_review = _file.readline()
             rating = int(_file.readline())
+            score = calculate_score(rating)
 
-            processed_review = list(preprocess_text(raw_review, word_transforms))
-            reviews_score.append((processed_review, rating))
+            # Ignore neutral reviews with rating=3 / score=0
+            if score != 0:
+                processed_review = list(preprocess_text(raw_review, word_transforms))
+                reviews_score.append((processed_review, score))
 
             # Jump blank line after rating
             # stop reading if end of file reached
@@ -24,3 +27,12 @@ def load_hotel_reviews(filepath, stem_words=False):
                 break
 
         return reviews_score
+
+
+def calculate_score(rating):
+    if rating <= 2:
+        return -1
+    elif rating >= 4:
+        return 1
+    else:
+        return 0
--- tests/flexical/socal/test_socal.py
+++ tests/flexical/socal/test_socal.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from unittest.case import TestCase
 
 from flexical.socal.socal import load_hotel_reviews
@@ -5,6 +6,6 @@ from flexical.socal.socal import load_hotel_reviews
 
 class LoadHotelReviews(TestCase):
     def test_return_correct_dict(self):
-        reviews = load_hotel_reviews('flexical/data/hotels.txt')
+        reviews = load_hotel_reviews(stem_words=True)
 
-        print reviews[:10]
+        print Counter(score for _, score in reviews)
