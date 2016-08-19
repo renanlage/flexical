29afcbbbcffca808ebd0c79b6e2e3fd85de03e0f
Renan Lage <renanlage@gmail.com>
2016-08-19 13:18:16 -0300

    Load sentilex and flexical lexicons

--- flexical/lexicons/__init__.py
+++ flexical/lexicons/__init__.py
@@ -14,10 +14,52 @@ def load_oplexicon(accepted_lex_types=('adj', 'emot'), stem_words=False):
 
     with io.open('flexical/lexicons/oplexicon.txt', 'r', encoding='utf-8') as _file:
         for line in _file:
-            word, lex_type, score, _ = line.split(',')
+            word, lex_type, score = line.split(',')[:3]
 
             if score != "0" and lex_type in accepted_lex_types:
                 word = apply_transforms(word, word_transforms)
                 lexicon[word] = int(score)
+
+    return lexicon
+
+
+def load_flexical(stem_words=False):
+    lexicon = {}
+
+    with io.open('flexical/lexicons/flexical.csv', 'r', encoding='utf-8') as _file:
+        for line in _file:
+            word, score = line.split(',')
+            polarity = float(score.rstrip())
+
+            if stem_words is True:
+                word = stem_word(word)
+
+            if polarity > 0:
+                lexicon[word] = 1
+            elif polarity < 0:
+                lexicon[word] = -1
+
+    return lexicon
+
+
+def load_sentilex(stem_words=False):
+    lexicon = {}
+
+    word_transforms = [remove_accents]
+
+    if stem_words is True:
+        word_transforms.append(stem_word)
+
+    with io.open('flexical/lexicons/sentilex.txt', 'r', encoding='latin-1') as _file:
+        for line in _file:
+            word, info = line.split('.')
+            polarity = int(info.split(';')[2].split('=')[-1])
+
+            if stem_words is True:
+                word = stem_word(word)
+
+            if polarity != 0:
+                word = apply_transforms(word, word_transforms)
+                lexicon[word] = polarity
 
     return lexicon
--- tests/flexical/lexicons/test_init.py
+++ tests/flexical/lexicons/test_init.py
@@ -1,15 +1,63 @@
-import os
 from unittest.case import TestCase
 
-from flexical.lexicons import load_oplexicon
+from flexical.lexicons import load_oplexicon, load_sentilex, load_flexical
 
 
 class LoadOplexiconTest(TestCase):
     def test_return_lexicon_in_expected_format(self):
-        lexicon = load_oplexicon(os.path.abspath('flexical/lexicons/oplexicon.txt'))
+        lexicon = load_oplexicon()
 
         self.assertIsInstance(lexicon, dict)
 
-        for entry in lexicon.iteritems():
+        for entry in lexicon.items()[:10]:
+            self.assertIsInstance(entry[0], unicode)
+            self.assertIn(entry[1], (-1, 1))
+
+    def test_return_stemmed_lexicon_in_expected_format(self):
+        lexicon = load_oplexicon(stem_words=True)
+
+        self.assertIsInstance(lexicon, dict)
+
+        for entry in lexicon.items()[:10]:
+            self.assertIsInstance(entry[0], unicode)
+            self.assertIn(entry[1], (-1, 1))
+
+
+class LoadSentilexTest(TestCase):
+    def test_return_lexicon_in_expected_format(self):
+        lexicon = load_sentilex()
+
+        self.assertIsInstance(lexicon, dict)
+
+        for entry in lexicon.items()[:10]:
+            self.assertIsInstance(entry[0], unicode)
+            self.assertIn(entry[1], (-1, 1))
+
+    def test_return_stemmed_lexicon_in_expected_format(self):
+        lexicon = load_sentilex(stem_words=True)
+
+        self.assertIsInstance(lexicon, dict)
+
+        for entry in lexicon.items()[:10]:
+            self.assertIsInstance(entry[0], unicode)
+            self.assertIn(entry[1], (-1, 1))
+
+
+class LoadFlexicalTest(TestCase):
+    def test_return_lexicon_in_expected_format(self):
+        lexicon = load_flexical(stem_words=False)
+
+        self.assertIsInstance(lexicon, dict)
+
+        for entry in lexicon.items()[:10]:
+            self.assertIsInstance(entry[0], unicode)
+            self.assertIn(entry[1], (-1, 1))
+
+    def test_return_stemmed_lexicon_in_expected_format(self):
+        lexicon = load_flexical(stem_words=True)
+
+        self.assertIsInstance(lexicon, dict)
+
+        for entry in lexicon.items()[:10]:
             self.assertIsInstance(entry[0], unicode)
             self.assertIn(entry[1], (-1, 1))
